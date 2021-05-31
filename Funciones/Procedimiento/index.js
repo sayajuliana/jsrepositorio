395ebd182b1eb52bb13345d1c0b0340df40d33ff
Juliana Leal <julianyta0325@hotmail.com>
2021-05-31 10:59:14 -0500

    ajuste

--- Funciones/Procedimiento/index.js
+++
@@ -1,9 +0,0 @@
-// cree un procedimiento que imprima un mensaje de bienvenida 
-
-// un procedimiento es una FUNCIÓN  que no retorna valores 
-
-function bienvenida (pais) {
-    let mensaje = `Bienvenido a  ${pais}`;
-    console.log(mensaje);
-}
-benvenida ("Colombia");
